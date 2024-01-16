e0a4c05c76969c1b57a6b72217dac6369ae0e369
MANSOUR ALDHAHRI <75687945+MansourAldhahri@users.noreply.github.com>
2024-01-16 16:57:17 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,3 @@
-    <!--name:Mansour Ahmed Aldhahri
-        ID:2036538
-        SECTION: CS1
-        
-    -->
-    <!--name:Musab Hassan Alsobhi
-        ID:2035566
-        SECTION: CS1
-    -->
 <!DOCTYPE html>
 <html>
 <head>
